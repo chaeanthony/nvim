cd8667deec4d2358b19ed5253ade4342fb81d674
Anthony Chae <chaeanthony21@gmail.com>
2025-07-24 11:10:13 -0400

    add keymaps for nav quickfix

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,2 +1,5 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down and center" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up and center" })
+
+vim.keymap.set("n", "<M-j>", "<cmd>cnext<CR>", { desc = "Next item in quickfix" })
+vim.keymap.set("n", "<M-k>", "<cmd>cprev<CR>", { desc = "Prev item in quickfix" })
